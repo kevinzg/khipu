6e1d41a0a05da37d528a3d3d77cd9b19806a4551
Kevin Zúñiga <kevin.zun@gmail.com>
2024-08-03 17:09:34 -0500

    feat: refactor score dial and don't reset points when dialing twice

--- src/lib/ui/Scoredial.svelte
+++ src/lib/ui/Scoredial.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { formatDelta } from '$lib/common';
+    import { formatDelta, assert } from '$lib/common';
     import type { Player } from '$lib/data.svelte';
 
     const FULL_TURN_POINTS = 20; // how many points is a complete rotation of the dial
@@ -11,47 +11,40 @@
 
     type Props = {
         players: Player[];
-        onpress: (idx: Player['id'], delta: number) => void; // callback when the button is pressed
+        onpress: (playerId: Player['id'], delta: number) => void; // callback when the button is pressed
     };
-
     const { players, onpress }: Props = $props();
 
-    let active = $state(false); // is the dial being turned?
-    let activePlayer = $state(0); // the player selected
-
-    // Position of hole being moved
-    let currentAngle = $state(0); // current angle of the dial being turned
-    let currentX = $derived(Math.cos(currentAngle) * DIAL_RADIUS);
-    let currentY = $derived(Math.sin(currentAngle) * DIAL_RADIUS);
-
-    let totalTurn = $state(0); // how much the dial has turned
-    let totalPoints = $derived(Math.round((totalTurn * FULL_TURN_POINTS) / (Math.PI * 2)));
-    let totalPointsLabel = $derived(formatDelta(totalPoints));
-
-    let currentColor = $derived(
-        totalTurn !== 0 && players.length > 0 ? players[activePlayer].color : null,
-    );
+    let activeHole: number | null = $state(null); // the hole selected
+    let activePlayer: Player | null = $state(null); // the current player for the active hole
+    let active = $derived(activeHole != null); // is the dial being turned?
 
     // Position for each button/hole
-    let buttons: { idx: number; x: number; y: number; name: string; color: string }[] = $derived.by(
-        () => {
-            const n = players.length;
-            return players.map((p, i) => {
-                const angle = (-i * Math.PI * 2) / n + OFFSET_ANGLE;
-                return {
-                    idx: i,
-                    name: p.name,
-                    color: p.color,
-                    x: Math.cos(angle) * DIAL_RADIUS,
-                    y: -Math.sin(angle) * DIAL_RADIUS,
-                };
-            });
-        },
-    );
+    let holes: { idx: number; angle: number; player: Player }[] = $derived.by(() => {
+        const n = players.length;
+        return players.map((p, i) => {
+            const angle = (-i * Math.PI * 2) / n + OFFSET_ANGLE;
+            return {
+                idx: i,
+                player: p,
+                angle,
+            };
+        });
+    });
+
+    let totalRotation = $state(0); // total rotation of the dial, goes beyond 2π
+    // FIXME: If I don't add (totalRotation * 0) here, the derived value doesn't **always** update, only some times
+    //        Looking at the totalRotation signal, it's reactions array doesn't contain the currentAngle derived signal
+    //        I believe this happens because totalRotation is not always read (depends on activeHole)
+    //        TODO: Create a minimal example to reproduce it and report it to Svelte
+    let currentAngle = $derived(
+        totalRotation * 0 + (activeHole != null ? holes[activeHole].angle - totalRotation : 0),
+    ); // angle of the active hole, including initial hole offset
+
+    let currentPoints = $state(0); // current points for the active player
+    let extraPoints = $derived(Math.round((totalRotation * FULL_TURN_POINTS) / (Math.PI * 2))); // points being added to the current points
 
     function setupEvents(svg: SVGElement) {
-        let prevAngle = 0;
-
         // Function to convert client coordinates to SVG coordinates
         const transform = ([clientX, clientY]: [number, number]) => {
             const bbox = svg.getBoundingClientRect();
@@ -73,56 +66,69 @@
             return [ev.clientX, ev.clientY];
         };
 
+        let prevAngle: number | null = null;
         const mouseMoveHandler = (ev: MouseEvent | TouchEvent) => {
             const [x, y] = transform(getCoords(ev));
             let angle = Math.atan2(y, x);
-            if (angle < 0) {
-                angle += Math.PI * 2;
+            if (prevAngle == null) {
+                prevAngle = angle;
+                return;
             }
-
             let delta = angle - prevAngle;
             prevAngle = angle;
-
             if (delta > Math.PI) {
                 delta -= Math.PI * 2;
             } else if (delta < -Math.PI) {
                 delta += Math.PI * 2;
             }
-
-            totalTurn += delta;
-            currentAngle = angle;
+            totalRotation += delta;
         };
 
         const mouseUpHandler = () => {
-            active = false;
-            currentAngle = 0;
+            resetState();
             window.removeEventListener('pointermove', mouseMoveHandler);
             window.removeEventListener('pointerup', mouseUpHandler);
         };
 
+        const resetState = () => {
+            activeHole = null;
+            prevAngle = null;
+            currentPoints += extraPoints;
+            totalRotation = 0; // implies extraPoints = 0
+            if (currentPoints == 0) {
+                activePlayer = null;
+            }
+        };
+
+        const mainButtonClicked = () => {
+            if (activePlayer == null) return;
+            if (active) return;
+            assert(extraPoints == 0, 'extra points should be 0 when pressing the main button');
+            onpress(activePlayer.id, currentPoints);
+            currentPoints = 0; // points were assigned, so reset to 0
+            resetState();
+        };
+
         const mouseDownHandler = (ev: MouseEvent | TouchEvent) => {
             ev.preventDefault(); // Disable scrolling for touch events
+
             // Main button
             if ((ev.target as SVGElement).getAttribute('data-main-button') === 'true') {
-                onpress(players[activePlayer].id, totalPoints);
-                totalTurn = 0;
+                mainButtonClicked();
                 return;
             }
 
             // Hole
             const rawIndex = (ev.target as SVGElement).getAttribute('data-player-index');
-            if (rawIndex == null || /^\d+$/.test(rawIndex) === false) {
-                return;
+            if (rawIndex != null) {
+                activeHole = parseInt(rawIndex);
+                if (activePlayer !== holes[activeHole].player) {
+                    currentPoints = 0; // Reset points if the player changed
+                }
+                activePlayer = holes[activeHole].player;
+                window.addEventListener('pointermove', mouseMoveHandler);
+                window.addEventListener('pointerup', mouseUpHandler);
             }
-            activePlayer = parseInt(rawIndex);
-            totalTurn = 0;
-            const [x, y] = transform(getCoords(ev));
-            const initialAngle = Math.atan2(y, x);
-            currentAngle = initialAngle;
-            prevAngle = initialAngle;
-            active = true;
-            window.addEventListener('pointermove', mouseMoveHandler);
-            window.addEventListener('pointerup', mouseUpHandler);
         };
         svg.addEventListener('pointerdown', mouseDownHandler);
         return {
@@ -156,7 +162,7 @@
                     r={CENTER_BUTTON_RADIUS}
                     stroke="black"
                     stroke-width="1"
-                    fill={currentColor ?? '#e2e8f0'}
+                    fill={activePlayer?.color ?? '#e2e8f0'}
                     data-main-button="true"
                 />
                 <!-- y=1 so text looks centered -->
@@ -171,55 +177,54 @@
                     font-weight="bold"
                     data-main-button="true"
                 >
-                    {totalPointsLabel}
+                    {formatDelta(currentPoints + extraPoints)}
                 </text>
             </g>
 
+            <!-- Holes -->
             {#if !active}
-                <!-- Holes for each player -->
-                {#each buttons as b}
-                    <g class="cursor-pointer">
-                        <circle
-                            cx={b.x}
-                            cy={b.y}
-                            r={HOLE_RADIUS}
-                            fill={b.color}
-                            stroke-width="0.5"
-                            stroke="#0f172a"
-                            data-player-index={b.idx}
-                        />
-                        <text
-                            x={b.x}
-                            y={b.y + 1}
-                            text-anchor="middle"
-                            dominant-baseline="middle"
-                            font-size="8"
-                            font-weight="bold"
-                            stroke="black"
-                            stroke-width="0.5"
-                            fill="white"
-                            data-player-index={b.idx}
-                        >
-                            {b.name.charAt(0)}
-                        </text>
-                    </g>
+                {#each holes as b}
+                    {@render hole(b.angle, b.idx, b.player)}
                 {/each}
             {/if}
 
             <!-- Moving circle -->
             {#if active}
-                <circle
-                    cx={currentX}
-                    cy={currentY}
-                    r={HOLE_RADIUS}
-                    stroke="#0f172a"
-                    stroke-width="0.5"
-                    fill={currentColor}
-                />
+                {@render hole(currentAngle, activeHole, activePlayer)}
             {/if}
         </g>
     </svg>
 </div>
+
+{#snippet hole(angle, index, player)}
+    {@const x = Math.cos(angle) * DIAL_RADIUS}
+    {@const y = -Math.sin(angle) * DIAL_RADIUS}
+    <g class="cursor-pointer">
+        <circle
+            cx={x}
+            cy={y}
+            r={HOLE_RADIUS}
+            fill={player.color}
+            stroke-width="0.5"
+            stroke="#0f172a"
+            data-player-index={index}
+        />
+        <text
+            {x}
+            y={y + 1}
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-size="8"
+            font-weight="bold"
+            stroke="black"
+            stroke-width="0.5"
+            fill="white"
+            data-player-index={index}
+        >
+            {player.name.charAt(0)}
+        </text>
+    </g>
+{/snippet}
 
 <style>
     .dial-container {
